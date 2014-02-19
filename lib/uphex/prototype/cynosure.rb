bac509d2d8fe70770cf0323007f7775b6f83f939
sashee <gsashee@gmail.com>
2014-02-19 13:18:27 +0100

    first

---
+++ lib/uphex/prototype/cynosure.rb
@@ -0,0 +1,9 @@
+require "uphex/prototype/cynosure/version"
+
+module Uphex
+  module Prototype
+    module Cynosure
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/uphex/prototype/cynosure/version.rb
@@ -0,0 +1,7 @@
+module Uphex
+  module Prototype
+    module Cynosure
+      VERSION = "0.0.1"
+    end
+  end
+end
